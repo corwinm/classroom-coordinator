3c5a71b789e5b34dcd3ac3f542ed40e308e43814
Corwin Marsh <corwinm@users.noreply.github.com>
2021-10-18 21:06:42 -0700

    refactor to use better names

--- src/excel/processOriginal.ts
+++ src/excel/processOriginal.ts
@@ -12,6 +12,10 @@ function createFileName(previous: string, start: string, end: string) {
   return `${parsedName} ${start} to ${end}`;
 }
 
+const lastWeekStartRow = 24;
+const lastWeekEndRow = 29;
+const thisWeekStartRow = 31;
+
 export default function processOriginal(file: File) {
   const reader = new FileReader();
   reader.readAsArrayBuffer(file);
@@ -45,17 +49,17 @@ export default function processOriginal(file: File) {
     rotation.spliceRows(2, 7);
 
     // Copy down last week to new week
-    rotation.getRows(24, 6)?.forEach((row, index) => {
+    rotation.getRows(lastWeekStartRow, 6)?.forEach((row, index) => {
       if (!rotation) {
         alert("Couldn't find worksheet named 'Rotation'");
         return;
       }
-      const copyRow = rotation.getRow(index + 32);
+      const copyRow = rotation.getRow(index + thisWeekStartRow);
       copyRow.height = row.height;
 
       for (const col of [2, 3, 4]) {
-        const cell = rotation.getCell(index + 24, col);
-        const copy = rotation.getCell(index + 31, col);
+        const cell = rotation.getCell(index + lastWeekStartRow, col);
+        const copy = rotation.getCell(index + thisWeekStartRow, col);
         copy.style = cell.style;
 
         if ([2, 4].includes(col)) {
@@ -67,12 +71,12 @@ export default function processOriginal(file: File) {
           } else if (index === 1) {
             let i = 0;
             while (cell.value && !copy.value && i < 5) {
-              const shifted = rotation.getCell(29 - i, col);
+              const shifted = rotation.getCell(lastWeekEndRow - i, col);
               copy.value = shifted.value;
               i += 1;
             }
           } else if (cell.value) {
-            const shifted = rotation.getCell(index + 23, col);
+            const shifted = rotation.getCell(index + lastWeekStartRow - 1, col);
             copy.value = shifted.value;
           }
           continue;
@@ -82,7 +86,7 @@ export default function processOriginal(file: File) {
       }
     });
     const start = parseDateFromHeaderCell(rotation.getCell(3, 2));
-    const end = parseDateFromHeaderCell(rotation.getCell(31, 4));
+    const end = parseDateFromHeaderCell(rotation.getCell(thisWeekStartRow, 4));
 
     const data = await workbook.xlsx.writeBuffer();
     const blob = new Blob([data], {
