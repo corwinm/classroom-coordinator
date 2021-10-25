9f35e8c75998df9715665e80f872a052e1c23517
Corwin Marsh <corwinm@users.noreply.github.com>
2021-10-24 23:05:35 -0700

    Add previous week being a holiday logic

--- src/excel/processOriginal.ts
+++ src/excel/processOriginal.ts
@@ -12,19 +12,25 @@ function createFileName(previous: string, start: string, end: string) {
   return `${parsedName} ${start} to ${end}`;
 }
 
-const lastWeekStartRow = 24;
-const lastWeekEndRow = 29;
+const lastWeekStartRowBase = 24;
+const lastWeekEndRowBase = 29;
 const thisWeekStartRow = 31;
 const tuesdayCol = 2;
 const choreCol = 3;
 const thursdayCol = 4;
+const noSchoolDefaultText = "No School";
 
 export default function processOriginal(
   file: File,
   {
     tuesdayHoliday,
     thursdayHoliday,
-  }: { tuesdayHoliday: boolean; thursdayHoliday: boolean }
+    noSchoolText = noSchoolDefaultText,
+  }: {
+    tuesdayHoliday: boolean;
+    thursdayHoliday: boolean;
+    noSchoolText?: string;
+  }
 ) {
   return new Promise<Workbook>((resolve, reject) => {
     const reader = new FileReader();
@@ -61,7 +67,35 @@ export default function processOriginal(
       rotation.spliceRows(2, 7);
 
       // Copy down last week to new week
-      rotation.getRows(lastWeekStartRow, 6)?.forEach((row, index) => {
+      let lastWeekStartRow: Record<number, number> = {
+        [tuesdayCol]: lastWeekStartRowBase,
+        [choreCol]: lastWeekStartRowBase,
+        [thursdayCol]: lastWeekStartRowBase,
+      };
+      let lastWeekEndRow: Record<number, number> = {
+        [tuesdayCol]: lastWeekEndRowBase,
+        [choreCol]: lastWeekEndRowBase,
+        [thursdayCol]: lastWeekEndRowBase,
+      };
+
+      const workingRows = rotation.getRows(lastWeekStartRowBase, 6);
+      // Check for last week being a holiday
+      for (const col of [tuesdayCol, thursdayCol]) {
+        let noSchoolTextFound = false;
+        do {
+          noSchoolTextFound = false;
+          workingRows?.forEach((row, index) => {
+            if (noSchoolTextFound) return;
+            const cell = rotation?.getCell(index + lastWeekStartRow[col], col);
+            if (cell?.value === noSchoolText) {
+              noSchoolTextFound = true;
+              lastWeekStartRow[col] = lastWeekStartRow[col] - 7;
+              lastWeekEndRow[col] = lastWeekEndRow[col] - 7;
+            }
+          });
+        } while (noSchoolTextFound);
+      }
+      workingRows?.forEach((row, index) => {
         if (!rotation) {
           reject("Couldn't find worksheet named 'Rotation'");
           return;
@@ -70,12 +104,13 @@ export default function processOriginal(
         copyRow.height = row.height;
 
         for (const col of [tuesdayCol, choreCol, thursdayCol]) {
-          const cell = rotation.getCell(index + lastWeekStartRow, col);
+          const cell = rotation.getCell(index + lastWeekStartRow[col], col);
           const copy = rotation.getCell(index + thisWeekStartRow, col);
           copy.style = cell.style;
 
           if ([tuesdayCol, thursdayCol].includes(col)) {
             if (index === 0) {
+              const cell = rotation.getCell(index + lastWeekStartRowBase, col);
               const [day, rawDate] = cell.value?.toString().split(" ") ?? [];
               const date = new Date(rawDate);
               date.setDate(date.getDate() + 7);
@@ -88,13 +123,13 @@ export default function processOriginal(
             } else if (index === 1) {
               let i = 0;
               while (cell.value && !copy.value && i < 5) {
-                const shifted = rotation.getCell(lastWeekEndRow - i, col);
+                const shifted = rotation.getCell(lastWeekEndRow[col] - i, col);
                 copy.value = shifted.value;
                 i += 1;
               }
             } else if (cell.value) {
               const shifted = rotation.getCell(
-                index + lastWeekStartRow - 1,
+                index + lastWeekStartRow[col] - 1,
                 col
               );
               copy.value = shifted.value;
